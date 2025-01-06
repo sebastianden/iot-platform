1625a693ade3f60c379390a4569b1ea02b5f855b
STDG <stdg@novonordisk.com>
2025-01-06 21:34:33 +0100

    feat: last value cards

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -12,14 +12,27 @@ export default function Home() {
     value: temperature,
     timestamp,
   }));
+
+  const lastTemperature =
+    temperature.length > 0
+      ? parseInt(temperature[temperature.length - 1].value)
+      : null;
+
   const humidity = data.map(({ humidity, timestamp }) => ({
     value: humidity,
     timestamp,
   }));
+
+  const lastHumidity =
+    humidity.length > 0 ? parseInt(humidity[humidity.length - 1].value) : null;
+
   const pressure = data.map(({ pressure, timestamp }) => ({
     value: pressure,
     timestamp,
   }));
+
+  const lastPressure =
+    pressure.length > 0 ? parseInt(pressure[pressure.length - 1].value) : null;
 
   return (
     <div className="flex p-20 font-[family-name:var(--font-geist-sans)]">
@@ -34,6 +47,15 @@ export default function Home() {
             />
           </div>
           <div className="p-4 border-2 shadow-md hover:shadow-lg border-gray-200 rounded-xl">
+            <p className="text-2xl text-gray-500">Temperature:</p>
+            <div className="flex justify-center">
+              <p className="flex font-bold text-5xl text-red-500">
+                {lastTemperature}
+              </p>
+              <p className="flex text-2xl text-gray-500">°C</p>
+            </div>
+          </div>
+          <div className="p-4 border-2 shadow-md hover:shadow-lg border-gray-200 rounded-xl">
             <LineChart
               data={humidity}
               label="Humidity (%)"
@@ -42,12 +64,30 @@ export default function Home() {
             />
           </div>
           <div className="p-4 border-2 shadow-md hover:shadow-lg border-gray-200 rounded-xl">
+            <p className="text-2xl text-gray-500">Humidity:</p>
+            <div className="flex justify-center">
+              <p className="flex font-bold text-5xl text-blue-500">
+                {lastHumidity}
+              </p>
+              <p className="flex text-2xl text-gray-500">%</p>
+            </div>
+          </div>
+          <div className="p-4 border-2 shadow-md hover:shadow-lg border-gray-200 rounded-xl">
             <LineChart
               data={pressure}
               label="Pressure (hPa)"
               borderColor="rgba(255, 159, 64, 1)"
               backgroundColor="rgba(255, 159, 64, 0.2)"
             />
+          </div>
+          <div className="p-4 border-2 shadow-md hover:shadow-lg border-gray-200 rounded-xl">
+            <p className="text-2xl text-gray-500">Pressure:</p>
+            <div className="flex justify-center">
+              <p className="flex font-bold text-5xl text-orange-500">
+                {lastPressure}
+              </p>
+              <p className="flex text-2xl text-gray-500">hPa</p>
+            </div>
           </div>
         </div>
       </main>
