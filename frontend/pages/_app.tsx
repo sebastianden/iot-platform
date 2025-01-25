5473f1f6491086bb165b17721b8acd97f5bec171
STDG <stdg@novonordisk.com>
2025-01-25 21:57:38 +0100

    feat: starting with amplify

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,28 @@
 import "@/styles/globals.css";
 import { AppProps } from "next/app";
-import { SessionProvider } from "next-auth/react";
+
+import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: "eu-central-1_WCbeFbGbp",
+      userPoolClientId: "2ab5b55urtmgve2qak6ei9u28v",
+    },
+  },
+});
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
 
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,5 @@
-import { useSession, signIn, signOut } from "next-auth/react";
 import Home from "./home";
+import Auth from "aws-amplify";
 
 export default function Component() {
   const { data: session } = useSession();
