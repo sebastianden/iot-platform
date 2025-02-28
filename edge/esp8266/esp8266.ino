b19bd1519e8aef8ce61c6a053bfb3d7e7bfa360c
STDG <stdg@novonordisk.com>
2025-02-28 17:10:22 +0100

    feat: added esp8266 code and instructions

---
+++ edge/esp8266/esp8266.ino
@@ -0,0 +1,109 @@
+// ESP8266 AWS IoT
+// Select NodeMCU 0.9 (ESP-12 Module) as board
+
+#include <string.h>
+#include "FS.h"
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include "DHT.h"
+#include "secrets.h"
+
+#define DHTTYPE DHT11
+#define DHTPIN D5
+
+// DHT11 temperature and humidity sensor
+DHT dht(DHTPIN, DHTTYPE); 
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
+
+// Callback triggered on recieving a message
+void callback(char* topic, byte* payload, unsigned int length) {
+}
+
+// Initialize WiFi and MQTT clients
+WiFiClientSecure espClient;
+PubSubClient client(AWS_endpoint, 8883, callback, espClient);
+char msg[100];
+
+// Load AWS certs
+BearSSL::X509List cert(cacert);
+BearSSL::X509List client_crt(client_cert);
+BearSSL::PrivateKey key(privkey);
+
+// Connect to WiFi
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  espClient.setBufferSizes(512, 512);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  timeClient.begin();
+  while(!timeClient.update()){
+    timeClient.forceUpdate();
+  }
+  espClient.setX509Time(timeClient.getEpochTime());
+}
+
+
+// Setup funtion, set up WiFi, load certificates, publish message
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+
+  dht.begin();
+  
+  // Initialize WiFi Connection
+  setup_wifi();
+  
+  delay(1000);
+
+  Serial.println("Trying to load AWS certificates");
+  // Update the WiFi Client with the AWS certificates
+  espClient.setTrustAnchors(&cert);
+  espClient.setClientRSACert(&client_crt, &key);
+  Serial.println("AWS certificates loaded");
+
+  Serial.print("Attempting MQTT connection...");
+  while (!client.connected()) {
+    // Attempt to connect
+    if (client.connect(AWS_thing)) {
+      Serial.println("connected");
+    }
+  }
+  // Get a time structure and extract year, month, day
+  unsigned long epochTime = timeClient.getEpochTime();
+  // Read sensor values
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  // Publish message
+  snprintf(msg, 100, "{\"timestamp\":%d,\n\"temperature\":%f,\n\"humidity\":%f}", epochTime, t, h);
+  client.publish(pub_topic, msg);
+  Serial.print("Publish message: ");
+  Serial.println(msg);
+
+  // Sleep
+  ESP.deepSleep(60*60*1e6);
+}
+
+// Dummy loop function, not used due to deepSleep
+void loop() {
+
+}
---
+++ edge/esp8266/secrets.h
@@ -0,0 +1,62 @@
+#define SECRET
+
+// Set WiFi credentials
+const char *ssid = "YOUR_WIFI_SSID";
+const char *password = "YOUR_WIFI_PASSWORD";
+
+// Set publish and subscribe topics
+const char *pub_topic = "YOUR_PUBLISH_TOPIC";
+
+// Set AWS Endpoint and Thing name
+const char *AWS_endpoint = "YOUR_AWS_ENDPOINT";
+const char *AWS_thing = "YOUR_THING_NAME";
+
+// AmazonRootCA1
+static const char cacert[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIDQTCCAimgAwIBAgITBmyfz5m/jAo54vB4ikPmljZbyjANBgkqhkiG9w0BAQsF
+ADA5MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6
+b24gUm9vdCBDQSAxMB4XDTE1MDUyNjAwMDAwMFoXDTM4MDExNzAwMDAwMFowOTEL
+MAkGA1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJv
+b3QgQ0EgMTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALJ4gHHKeNXj
+ca9HgFB0fW7Y14h29Jlo91ghYPl0hAEvrAIthtOgQ3pOsqTQNroBvo3bSMgHFzZM
+9O6II8c+6zf1tRn4SWiw3te5djgdYZ6k/oI2peVKVuRF4fn9tBb6dNqcmzU5L/qw
+IFAGbHrQgLKm+a/sRxmPUDgH3KKHOVj4utWp+UhnMJbulHheb4mjUcAwhmahRWa6
+VOujw5H5SNz/0egwLX0tdHA114gk957EWW67c4cX8jJGKLhD+rcdqsq08p8kDi1L
+93FcXmn/6pUCyziKrlA4b9v7LWIbxcceVOF34GfID5yHI9Y/QCB/IIDEgEw+OyQm
+jgSubJrIqg0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMC
+AYYwHQYDVR0OBBYEFIQYzIU07LwMlJQuCFmcx7IQTgoIMA0GCSqGSIb3DQEBCwUA
+A4IBAQCY8jdaQZChGsV2USggNiMOruYou6r4lK5IpDB/G/wkjUu0yKGX9rbxenDI
+U5PMCCjjmCXPI6T53iHTfIUJrU6adTrCC2qJeHZERxhlbI1Bjjt/msv0tadQ1wUs
+N+gDS63pYaACbvXy8MWy7Vu33PqUXHeeE6V/Uq2V8viTO96LXFvKWlJbYK8U90vv
+o/ufQJVtMVT8QtPHRh8jrdkPSHCa2XV4cdFyQzR1bldZwgJcJmApzyMZFo6IQ6XU
+5MsI+yMRQ+hDKXJioaldXgjUkK642M4UwtBV8ob2xJNDd2ZhwLnoQdeXeGADbkpy
+rqXRfboQnoZsG4q5WTP468SQvvG5
+-----END CERTIFICATE-----
+)EOF";
+
+// Copy contents from certificate.pem.crt here ▼
+static const char client_cert[] PROGMEM = R"KEY(
+-----BEGIN CERTIFICATE-----
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+-----END CERTIFICATE-----
+)KEY";
+
+// Copy contents from private.pem.key here ▼
+static const char privkey[] PROGMEM = R"KEY(
+-----BEGIN RSA PRIVATE KEY-----
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+-----END RSA PRIVATE KEY-----
+)KEY";
